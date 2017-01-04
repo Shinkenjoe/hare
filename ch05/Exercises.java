9b4b4c044875019699dc738be09da6468d787e34
Simon Mayer <simomayer@gmail.com>
2017-01-04 13:56:49 +0100

    5.4 Reading Floats with Error Codes and not Exceptions

--- ch05/Exercises.java
+++ ch05/Exercises.java
@@ -37,18 +37,90 @@ of the inputs are not floating-point numbers.*/;
 	    "/impatient/done/ch05/none-floats";
 	System.out.println("read fail file: " + filename);
 	try{
-	result = FloatReader.readValues(filename);
+	    result = FloatReader.readValues(filename);
 	} catch (NumberFormatException exc) {
 	    System.out.println("NumberFormatException caught by caller.");
 	}  catch (IOException exc) {
-	System.out.println("other caught by caller.");
-    }
+	    System.out.println("other caught by caller.");
+	}
 
 	/*     Write a method public double sumOfValues(String filename)
 	       throws... that calls the preceding method and returns the
 	       sum of the values in the file. Propagate any exceptions
 	       to the caller.
 	*/
-    }    
+	filename = "/home/shinken/comp/java/core"+
+	    "/impatient/done/ch05/floats";
+	System.out.println("get sum of floats in: " + filename);
+	double result2 = 0;
+	try {
+	    result2 = FloatReader.sumOfValues(filename);
+	} catch (NumberFormatException | IOException  exc) {
+	    System.out.println("other caught by caller.");
+	}    
+	System.out.println(result2);
+
+	/*3. Write a program that calls the preceding method and
+	  prints the result. Catch the exceptions and provide feedback
+	  to the user about any error conditions.
+	  is already Done.
+	*/
+	/*4. Repeat the preceding exercise, but don’t use exceptions.
+	  Instead, have readValues and sumOfValues return error codes
+	  of some kind. */
+
+	filename = "/home/shinken/comp/java/core"+
+	    "/impatient/done/ch05/floats";
+	System.out.println("EC read Float file: " + filename);
+	result = FloatReader.readValuesEC(filename);
+	FloatReader.reportResult(result);
+	
+	System.out.println("EC read nonexisting file");
+	result = FloatReader.readValuesEC("/jaerer");
+	FloatReader.reportResult(result);	
+	
+	filename = "/home/shinken/comp/java/core"+
+	    "/impatient/done/ch05/none-floats";
+	System.out.println("EC read fail file: " + filename);
+	result = FloatReader.readValuesEC(filename);
+	FloatReader.reportResult(result);
+
+
+	filename = "/home/shinken/comp/java/core"+
+	    "/impatient/done/ch05/floats";
+	System.out.println("get ECsum of floats in: " + filename);
+	result2 = FloatReader.sumOfValuesEC(filename);
+	FloatReader.reportResult2(result2);
+
+	/*5. Implement a method that contains the code with a Scanner and 
+	  a PrintWriter in Section 5.1.5, “The Try-with-Resources Statement,”
+	  on p. 179. But don’t use the try-with-resources statement. Instead,
+	  just use catch clauses. Be sure to close both objects, provided they
+	  # have been properly constructed. You need to consider the
+	  following conditions:
+	  • The Scanner constructor throws an exception.
+	  • The PrintWriter constructor throws an exception.
+	  • hasNext, next, or println throw an exception.
+	  • out.close() throws an exception.
+	  • in.close() throws an exception.
+	*/
+	
+	ArrayList<String> lines = ...;
+	try (PrintWriter out = new PrintWriter(“output.txt”)) {
+	    for (String line : lines) {
+		out.println(line.toLowerCase());
+	    }
+	}
+
+	try (Scanner in = new Scanner(Paths.get(“/usr/share/dict/words”));
+	     PrintWriter out = new PrintWriter(“output.txt”)) {
+	    while (in.hasNext())
+		out.println(in.next().toLowerCase());
+	}
+
+
+
+    }
+    
 }
 
--- ch05/FloatReader.java
+++ ch05/FloatReader.java
@@ -47,5 +47,91 @@ public class FloatReader {
 	    to the caller.
     */
 
-}
+    public static double sumOfValues(String filename)
+	throws IOException, NumberFormatException {
+	ArrayList<Double> arr = FloatReader.readValues(filename);
+	double res = 0;
+	for (Double numba : arr){
+	    res += numba;
+	}
+	return res;
+    }
 
+    /*4. Repeat the preceding exercise, but don’t use exceptions.
+      Instead, have readValues and sumOfValues return error codes
+      of some kind.*/
+
+
+    public static ArrayList<Double> readValuesEC(String filename)
+    {
+	ArrayList<Double> result = new ArrayList<Double>();
+	FileReader filereader;
+	BufferedReader reader;
+	try {
+	    reader = new BufferedReader(new FileReader(filename));  
+	    String line;
+	    String[] stringNumbas;	    
+	    while ((line = reader.readLine()) != null){
+		stringNumbas = line.split(" ");
+		for (String numba : stringNumbas){
+		    result.add(new Double(numba));
+		}
+	    }
+	} catch (FileNotFoundException exc) {
+	    System.out.println("File not found.\n");
+	    return new ArrayList<Double>(Arrays.asList(-1.0, 0.0));
+	} catch (IOException exc) {
+	    System.out.println("Problems reading a line.\n");
+	    return new ArrayList<Double>(Arrays.asList(-1.0, 1.0));
+	} catch (NumberFormatException exc) {
+	    System.out.println("File has non-float elements.\n");
+	    return new ArrayList<>(Arrays.asList(-1.0, 2.0));
+	} 
+      	return result;	
+    }
+
+    public static double sumOfValuesEC(String filename){
+	ArrayList<Double> arr = FloatReader.readValuesEC(filename);
+	double res = 0;
+	for (Double numba : arr){
+	    res += numba;
+	}
+	return res;
+    }
+    
+    public static void reportResult(List<Double> result){
+	if (result.get(0) == -1){
+	    switch ((result.get(1).intValue())) {
+	    case 0:
+		System.out.println("File not found");
+		break;
+	    case 1:
+		System.out.println("Problems with reading a file.");
+		break;
+	    case 2:
+		System.out.println("Non-float stuff in file");
+		break;
+	    default:
+		System.out.println(result.toString());
+	    } 	    
+	    
+	} else
+	    System.out.println(result.toString());
+    }
+    
+    public static void reportResult2(Double result2){
+	switch (result2.intValue()) {
+	case -1:
+	    System.out.println("File not found");
+	    break;
+	case 0:
+	    System.out.println("Problems with reading a file.");
+	    break;
+	case 1:
+	    System.out.println("Non-float stuff in file");
+	    break;
+	default:
+	    System.out.println(result2);
+	}
+    }
+}
